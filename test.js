3f26c21f7bc64b06513ecd490b9550220f6ffcf1
Mikel Tejero de la Fuente <mikeltejerodelafuente@MacBook-Air-de-Mikel.local>
2018-02-05 18:08:35 +0100

    Ordenar codigo y añadir comentarios

--- test.js
+++ test.js
@@ -4,11 +4,14 @@ const rootPage = "http://www.policlinicagipuzkoa.com/buscar-nacimiento/page/";
 
 
 let scrape = async (index) => {
+    
+    //Abrir pagina 
     const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
     await page.goto(rootPage+index,{timeout:0});
+    
+    //Seleccionar elementos
     const result = await page.evaluate(() => {
-        
         let elements = document.querySelectorAll('.datos-nacimientos'); 
         let data = [];
         for (var element of elements){ 
@@ -24,14 +27,10 @@ let scrape = async (index) => {
     return result; 
 };
 
-
-
-  
+//Repetir ciclo sobre i paginas
+for(i=0;i<8;i++){
+    scrape(i).then((value) => {
+        console.log(value); 
     
-
-    for(i=0;i<8;i++){
-        scrape(i).then((value) => {
-            console.log(value); 
-        
-        })
-    }
+    })
+}
